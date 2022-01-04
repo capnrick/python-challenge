a9f2c93f3c5871c38ea5814a02d7b32322ccf78b
capnrick <herrric@hawk.iit.edu>
2022-01-03 22:57:36 -0600

    Re working code for PyBank assignment. Adding results.text output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,73 +7,90 @@ a_file = "Resources/budget_data.csv"
 csvpath= os.path.join(a_file)
 total_rows = 0
 total = 0
-top_increase=["",0]
-top_decrease=["",9999999999]
+profit_loss=[]
+date_list=[]
 
 
+#open the csv file and begin reading in files
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #header row is not being read
     header_row = next(csvreader)
     
-    row_zero = next(csvreader)
 
-    #previous_PL_value=int(row[1])
-
+    #loop through csvfile to obtain number of months and to add up all values
     for row in csvreader:
-        #print rows to screen
+        #print rows to screen to verify data is being read
         print(row)
-        #increment counter each time to count number of rows(months)
-        total_rows=total_rows+1
-        
-        
-        previou_PL_value=int(row[1])
-        #loop through rows and add the second column's values up to get a total 
-        total += int(row[1])
-
-
-         #The greatest increase in profits (date and amount) over the entire period  
-        #only do the first comparison
-        net_change = int(row[1]) - int(row_zero[1])
-        #if net change()> top increase, then net change equal to top increase
-        
+               
+        #store values into array called profit_lossand date_list, start at second row to skip header
+        date_list.append(row[0])
+        profit_loss.append(row[1])
         
 
+        #add up all the values in the profit and loss columns and save net total  to variable 'total'
+        #same as writing total= total + int(row[1])
+        total+= int(row[1])
+    
+    #calculate and print the total number of months included in the dataset and net profit/loss
+    total_rows=len(profit_loss)
+    print(f'The total number of months is {total_rows}') 
+    print(f'The total net amount of Profit/Loss over the entire period is ${total}')
+
+    sum_changes=0
+    greatest_increase=0
+    greatest_decrease=0
+    #Calculate the changes in Profit/Losses" over the entire period, then find the average of those changes
+    for month in range(1,total_rows):
         
-            
+       
 
-    #print the total number of months included in the dataset
-    print (total_rows)   
+        #find the change in pnl from month to month
+        monthly_change = int(profit_loss[month])-int(profit_loss[month-1])
+        #print(monthly_change)
+    
+        sum_changes +=monthly_change
 
-    #print the total of all added up profit/loss
-    print(f'The total amount of Profit/Loss is {total}')
+        #average of all monthly changes
+        average_changes=(sum_changes/(total_rows-1))
+        
+
+    #The greatest increase in profits (date and amount) over the entire period
+    #The greatest decrease in profits (date and amount) over the entire period
+
+        if(monthly_change > greatest_increase):
+            greatest_increase=monthly_change
+            greatest_increase_month=date_list[month]
+        elif (monthly_change < greatest_decrease):
+            greatest_decrease=monthly_change
+            greatest_decrease_month=date_list[month]    
 
 
-
-
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-    average_PL = total/total_rows
-    print (average_PL)
-
-
-#The greatest decrease in profits (date and amount) over the entire period
+   
+          
+print(f'The average of the changes in Profit/Loss is ${round(average_changes,2)}')
+print(f"Greatest Increase in Profits:{greatest_increase_month} ${greatest_increase}")
+print(f"Greatest Decrease in Profits:{greatest_decrease_month} ${greatest_decrease}")
 #in addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
 
+
+
+#-------writing to the results to a text file--------------------------------
 # Specify the file to write to
 output_path = os.path.join("analysis/results.txt")
-
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w', newline='') as file:
     months=str(total_rows)
-    average=str(average_PL)
+    average=str(round(average_changes,2))
     totals=str(total)
-    # Initialize csv.writer
-    #csvwriter = csv.writer(csvfile, delimiter=',')
+    greatest_increase=str(greatest_increase)
+    greatest_decrease=str(greatest_decrease)
 
-    # Write the first row (column headers)
-    #file.write(['First Name', 'Last Name', 'SSN'])
+#write stored variables to file to be called results.txt
     file.write("Financial Analysis \n")
     file.write("---------------------------- \n")
     file.write("Total Months: ")
@@ -84,6 +101,17 @@ with open(output_path, 'w', newline='') as file:
     file.write ("\n")
     file.write("Average Change: $")
     file.write(average)
+    file.write ("\n")
+    file.write("Greatest Increase in Profits:")
+    file.write(greatest_increase_month)
+    file.write(" $")
+    file.write(greatest_increase)
+    file.write ("\n")
+    file.write("Greatest Decrease in Profits:")
+    file.write(greatest_decrease_month)
+    file.write(" $")
+    file.write(greatest_decrease)
+    
    
 
 #sample output
